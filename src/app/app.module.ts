d75419c719409e8298e755bbb9b6b8224aa8b796
Fernando Paredes <likkowg@gmail.com>
2018-02-23 17:18:50 -0500

    Imported from Original without changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,26 +5,66 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+
+
+// AF2 Settings
+export const firebaseConfig = {
+  apiKey: "AIzaSyBiG1zQ9qFoZhXTGm9zRrFNUAxXC9XDCiA",
+  authDomain: "igotaxi-70231.firebaseapp.com",
+  databaseURL: "https://igotaxi-70231.firebaseio.com",
+  projectId: "igotaxi-70231",
+  storageBucket: "igotaxi-70231.appspot.com",
+  messagingSenderId: "84785757637"
+};
+
+// Import the AF2 Module
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+import { Geolocation } from '@ionic-native/geolocation';
+import { DriverService } from '../services/driver-service';
+import { TransactionService } from '../services/transaction-service';
+import { ReportService } from '../services/report-service';
+import { PlaceService } from '../services/place-service';
+import { DealService } from '../services/deal-service';
+import { TripService } from '../services/trip-service';
+import { AuthService } from '../services/auth-service';
+import { SettingService } from '../services/setting-service';
+import { HttpModule } from '@angular/http';
+import { IonicStorageModule } from '@ionic/storage';
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    HttpModule,
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot(),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    Geolocation,
+    DriverService,
+    ReportService,
+    TransactionService,
+    PlaceService,
+    DealService,
+    TripService,
+    AuthService,
+    SettingService,
   ]
 })
 export class AppModule {}
